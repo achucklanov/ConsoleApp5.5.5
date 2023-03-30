94bae5ed414dd96c336afc684a4e4fcfd617e4a8
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-31 00:22:49 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp5.5.5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp5._5._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте!");
+
+            static int Factorial(int x)
+            {
+                x = 20;
+
+                if (x == 0)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return x * Factorial(x - 1);
+                }
+            }
+
+        }
+    }
+}
